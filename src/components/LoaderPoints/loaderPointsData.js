24f06e16c2542b6a7ed893760a0b62afabdf5732
hughb1oh1 <hughb1oh1@gmail.com>
2025-02-26 10:33:45 +1100

    changed loader points to separate json

---
+++ src/components/LoaderPoints/loaderPointsData.js
@@ -0,0 +1,54 @@
+const loaderPointsData = [
+    {
+      id: 1,
+      title: "WORKING COMFORTABLY",
+      description: "A spacious and comfortable working environment, 360 degree vision."
+    },
+    {
+      id: 2,
+      title: "WORK SAFELY",
+      description: "The cabin and safety roof are ROPS/FOPS certified CE international standard."
+    },
+    {
+      id: 3,
+      title: "LITHIUM ION PHOSPHATE BATTERY",
+      description: "A safe and efficient lithium ion phosphate battery has been carefully designed for our machines. This battery contains new blade cell lithium technology. Powered by BYD®Blade Battery Technology or AGM high capacity all-temperature batteries."
+    },
+    {
+      id: 4,
+      title: "MAINTENANCE",
+      description: "Due to the maintenance-free battery and electric motors, the maintenance intensity of these machines is lower compared to fuel-driven machines"
+    },
+    {
+      id: 5,
+      title: "SPEED",
+      description: "2 Driving modes. Easily selectable (0-10m/h, 0-20 km/h)."
+    },
+    {
+      id: 6,
+      title: "MANEUVERABILITY AND STABILITY",
+      description: "The articulated pendulum system ensures manoeuvrability and stability with dual motor drive all-terrain traction."
+    },
+    {
+      id: 7,
+      title: "MULTIFUNCTIONAL",
+      description: "Switch attachments easily and adapt the machine to your needs. Euro hitch quick change coupling."
+    },
+    {
+      id: 8,
+      title: "POWERFUL & PRODUCTIVE",
+      description: "Equipped with 2 independent electric motors for an optimal and efficient drive system."
+    },
+    {
+      id: 9,
+      title: "SILENT OPERATION",
+      description: "Ideal for working in quiet areas to avoid noise pollution. Perfect for urban areas and around livestock."
+    },
+    {
+      id: 10,
+      title: "COMPACT DESIGN",
+      description: "The compact design of Enloq Loaders® makes it convenient to work in places with limited space. Articulated steering delivers a tight turning radius."
+    }
+  ];
+  
+  export default loaderPointsData;
