5c76066b1bd2a52b4b6270ca136d30d4e126c256
hughb1oh1 <hughb1oh1@gmail.com>
2025-02-26 15:06:59 +1100

    Section Events update with info

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import './Event.css';
 
+import event6Image from '../../assets/images/event_wimmera_field_days.png'
+import event1Image from '../../assets/images/event_farm_world.png'
+import event2Image from '../../assets/images/event_seymour_expo.png'
+import event3Image from '../../assets/images/event_east_gippsland_field_days.png'
+import event4Image from '../../assets/images/event_tocal_field_days.png'
+import event5Image from '../../assets/images/event_norco_primex.png'
+
+
 const Event = () => {
   const events = [
     {
+      id: 6,
+      title: '',
+      details: '4-6 March 2025, Longerenong VIC',
+      image: event6Image
+    },
+    {
       id: 1,
-      title: 'Event title',
-      details: 'Dates and address etc',
-      image: '/api/placeholder/300/200'
+      title: '',
+      details: '27- 29 March 2025, Lardner Park VIC',
+      image: event1Image
     },
     {
       id: 2,
-      title: 'Event title',
-      details: 'Dates and address etc',
-      image: '/api/placeholder/300/200'
+      title: '',
+      details: '4 - 6 April 2025, Kings Park Seymour VIC',
+      image: event2Image
     },
     {
       id: 3,
-      title: 'Event title',
-      details: 'Dates and address etc',
-      image: '/api/placeholder/300/200'
+      title: '',
+      details: '11-12 April 2025, Bairnsdale VIC',
+      image: event3Image
+    },
+    {
+      id: 4,
+      title: '',
+      details: '2-4 May 2025, Tocal NSW',
+      image: event4Image
+    },
+    {
+      id: 5,
+      title: '',
+      details: '15-17 May 2025, Casino NSW',
+      image: event5Image
     }
   ];
 
